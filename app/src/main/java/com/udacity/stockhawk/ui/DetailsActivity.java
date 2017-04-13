4cde0cb1f9bea73f32620e0f24b4a9bce8c678ea
davegogol <g.davide1989@gmail.com>
2017-04-13 22:15:05 +0200

    adding chart displaying and data navigation

--- app/src/main/java/com/udacity/stockhawk/ui/DetailsActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailsActivity.java
@@ -1,12 +1,15 @@
 package com.udacity.stockhawk.ui;
 
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import com.udacity.stockhawk.R;
@@ -14,19 +17,37 @@ import com.udacity.stockhawk.data.Contract;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
 import timber.log.Timber;
 
 public class DetailsActivity extends AppCompatActivity
-        implements LoaderManager.LoaderCallbacks<Cursor> {
+        implements LoaderManager.LoaderCallbacks<Cursor>,
+        TabHost.OnTabChangeListener{
     private static final int ID_DETAIL_LOADER = 353;
+    private static final String Y_AXIS_DATE_FORMAT = "yyyy-MM-dd";
+
     private DecimalFormat percentageFormat;
 
     private String symbol;
     private Uri mUri;
+    private int limit = 5;
 
     @SuppressWarnings("WeakerAccess")
     @BindView(R.id.textViewStockPrice)
@@ -36,12 +57,22 @@ public class DetailsActivity extends AppCompatActivity
     @BindView(R.id.textViewStockMargin)
     TextView textViewStockMargin;
 
+    @BindView(android.R.id.tabhost)
+    TabHost mTabHost;
+
+    @BindView(android.R.id.tabcontent)
+    View mTabContent;
+
+    @BindView(R.id.stock_chart)
+    LineChartView mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         ButterKnife.bind(this);
+
+        setupTabs();
 
         percentageFormat = (DecimalFormat) NumberFormat.getPercentInstance(Locale.getDefault());
         percentageFormat.setMaximumFractionDigits(2);
@@ -113,10 +144,119 @@ public class DetailsActivity extends AppCompatActivity
                 String.valueOf(absoluteChange) + " ("+
                 percentage + ")"
         );
+
+        String historicalData = data.getString(
+                data.getColumnIndex(Contract.Quote.COLUMN_HISTORY)
+        );
+        updateChart(historicalData);
     }
 
     @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
+    public void onLoaderReset(Loader<Cursor> loader) {}
 
+    private void setupTabs() {
+
+        mTabHost.setup();
+
+        TabHost.TabSpec tabSpec;
+        tabSpec = mTabHost.newTabSpec(getString(R.string.stock_detail_tab_5_days));
+        tabSpec.setIndicator(getString(R.string.stock_detail_tab_5_days));
+        tabSpec.setContent(android.R.id.tabcontent);
+        mTabHost.addTab(tabSpec);
+
+        tabSpec = mTabHost.newTabSpec(getString(R.string.stock_detail_tab_2_weeks));
+        tabSpec.setIndicator(getString(R.string.stock_detail_tab_2_weeks));
+        tabSpec.setContent(android.R.id.tabcontent);
+        mTabHost.addTab(tabSpec);
+
+        tabSpec = mTabHost.newTabSpec(getString(R.string.stock_detail_tab_1_month));
+        tabSpec.setIndicator(getString(R.string.stock_detail_tab_1_month));
+        tabSpec.setContent(android.R.id.tabcontent);
+        mTabHost.addTab(tabSpec);
+
+        mTabHost.setOnTabChangedListener(this);
+
+        mTabHost.setCurrentTab(0);
+
+    }
+
+    private void updateChart(String historicalData) {
+
+        String[] stocksPricePerDayArray = historicalData.split("\n");
+        Map<String,String> stockPerDayMap = new HashMap<>();
+
+        for(String stockPricePerDay : Arrays.copyOfRange(stocksPricePerDayArray, 0, limit)){
+            String[] stockPricePerDayArray = stockPricePerDay.split(", ");
+            stockPerDayMap.put(
+                    stockPricePerDayArray[0],
+                    stockPricePerDayArray[1]);
+        }
+
+        List<AxisValue> axisValuesX = new ArrayList<>();
+        List<PointValue> pointValues = new ArrayList<>();
+
+        int counter = -1;
+        int stockPerDayMapSize = stockPerDayMap.size();
+
+        Iterator iterator = stockPerDayMap.keySet().iterator();
+
+        do {
+            counter++;
+
+            String dateInMillis = (String) iterator.next();
+            String bidPrice = stockPerDayMap.get(dateInMillis);
+
+            SimpleDateFormat dateFormat =
+                    new SimpleDateFormat(Y_AXIS_DATE_FORMAT, Locale.getDefault());
+            String formattedDate =
+                    dateFormat.format(new Date(Long.valueOf(
+                            dateInMillis
+                    )));
+
+            int x = stockPerDayMapSize - 1 - counter;
+
+            PointValue pointValue = new PointValue(x, Float.valueOf(bidPrice));
+            pointValue.setLabel(formattedDate);
+            pointValues.add(pointValue);
+
+            if (counter != 0 && counter % (stockPerDayMapSize / 3) == 0) {
+                AxisValue axisValueX = new AxisValue(x);
+                axisValueX.setLabel(formattedDate);
+                axisValuesX.add(axisValueX);
+            }
+
+        } while (iterator.hasNext());
+
+        Line line = new Line(pointValues).setColor(Color.WHITE).setCubic(false);
+
+        List<Line> lines = new ArrayList<>();
+        lines.add(line);
+        LineChartData lineChartData = new LineChartData();
+        lineChartData.setLines(lines);
+
+        Axis axisX = new Axis(axisValuesX);
+        axisX.setHasLines(true);
+        axisX.setMaxLabelChars(4);
+        lineChartData.setAxisXBottom(axisX);
+
+        Axis axisY = new Axis();
+        axisY.setAutoGenerated(true);
+        axisY.setHasLines(true);
+        axisY.setMaxLabelChars(4);
+        lineChartData.setAxisYLeft(axisY);
+
+        mChart.setInteractive(false);
+        mChart.setLineChartData(lineChartData);
+
+        mChart.setVisibility(View.VISIBLE);
+        mTabContent.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onTabChanged(String tabId) {
+        if(tabId.equals(getString(R.string.stock_detail_tab_2_weeks))) limit = 14;
+        else if(tabId.equals(getString(R.string.stock_detail_tab_5_days))) limit = 5;
+        else if(tabId.equals(getString(R.string.stock_detail_tab_1_month))) limit = 30;
+        getSupportLoaderManager().restartLoader(ID_DETAIL_LOADER, null, this);
     }
 }
